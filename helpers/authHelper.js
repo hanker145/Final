8e63139ab818ecf30f543292a9307b7e276d2602
hanker145 <phamvangiau2311@gmail.com>
2024-03-28 16:10:13 +0700

    yes1

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,15 +1,15 @@
-import bcrypt from 'bcrypt'
+import bcryptjs from "bcryptjs";
 
 export const hashPassword = async (password) => {
-    try {
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        return hashedPassword
-    } catch (error) {
-        consolelog(error)
-    }
-}
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcryptjs.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    consolelog(error);
+  }
+};
 
 export const comparePassword = async (password, hashedPassword) => {
-    return bcrypt.compare(password, hashedPassword)
-}
+  return bcrypt.compare(password, hashedPassword);
+};
