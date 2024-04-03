1eb7d092de4c34b4168b94fbebc94dea439b72ab
hanker145 <phamvangiau2311@gmail.com>
2024-04-03 16:15:41 +0700

    urr

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -166,7 +166,7 @@ const HomePage = () => {
             {products?.map((p) => (
               <div className="card m-2" key={p._id}>
                 <img
-                  src={`images/${p._id}`}
+                  src={`/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
                 />
